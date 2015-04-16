5ebf093e9b28f3be7c6f52f3b7391fbdbb001e5f
SuperRedNova <superrednova42@gmail.com>
2015-04-15 21:04:14 -0700

    Bug fixing
    woot!

--- VariableTexture.cs
+++ VariableTexture.cs
@@ -8,37 +8,25 @@ namespace KSPModelRocketry
 {
     public class VariableTexture : PartModule
     {
+        [Persistent]
+        Color color1 = new Color(1, 1, 1);
+        [Persistent]
+        Color color2 = new Color(0, 0, 0);
+
         [KSPField(guiActiveEditor = true, guiName = "Red", guiFormat = "F2")]
         [UI_FloatRange(minValue = 0, maxValue = 1, stepIncrement = .05f)]
         public float red = 1f;
-        [KSPField(isPersistant = true)]
-        private float red1;
-        [KSPField(isPersistant = true)]
-        private float red2 = 0f;
 
         [KSPField(guiActiveEditor = true, guiName = "Green", guiFormat = "F2")]
         [UI_FloatRange(minValue = 0, maxValue = 1, stepIncrement = .05f)]
         public float green = 1f;
-        [KSPField(isPersistant = true)]
-        private float green1;
-        [KSPField(isPersistant = true)]
-        private float green2 = 0f;
 
         [KSPField(guiActiveEditor = true, guiName = "Blue", guiFormat = "F2")]
         [UI_FloatRange(minValue = 0, maxValue = 1, stepIncrement = .05f)]
         public float blue = 1f;
-        [KSPField(isPersistant = true)]
-        private float blue1;
-        [KSPField(isPersistant = true)]
-        private float blue2 = 0f;
 
-        [KSPField]
-        public string textureMeshName = "Variable";
-        private Renderer texRend;
-        private Material mat;
-
-        [KSPField(guiActiveEditor = true, guiName = "Edit Color:", isPersistant = true)]
-        [UI_Toggle(enabledText="Primary",disabledText="Secondary")]
+        [KSPField(guiActiveEditor = true, guiName = "Edit Color:")]
+        [UI_Toggle(enabledText = "Primary", disabledText = "Secondary")]
         public bool switchColor = true;
         private bool swtclr;
 
@@ -54,6 +42,11 @@ namespace KSPModelRocketry
         public float texRep;
         private bool[,] pattern = new bool[2, 2];
 
+        [KSPField]
+        public string textureMeshName = "Variable";
+        private Renderer texRend;
+        private Material mat;
+
         /// <summary>
         /// Raises the start event.
         /// </summary>
@@ -65,31 +58,70 @@ namespace KSPModelRocketry
             base.OnAwake();
         }
 
+        public override void OnLoad(ConfigNode node)
+        {
+            if (node.GetNode("color1") != null)
+            {
+                ConfigNode c1 = node.GetNode("color1");
+                color1 = new Color(Convert.ToSingle(c1.GetValue("red")),
+                    Convert.ToSingle(c1.GetValue("green")),
+                    Convert.ToSingle(c1.GetValue("blue")));
+                red = color1.r;
+                green = color1.g;
+                blue = color1.b;
+                switchColor = true;
+            }
+            if (node.GetNode("color2") != null)
+            {
+                ConfigNode c2 = node.GetNode("color2");
+                color2 = new Color(Convert.ToSingle(c2.GetValue("red")),
+                    Convert.ToSingle(c2.GetValue("green")),
+                    Convert.ToSingle(c2.GetValue("blue")));
+            }
+            base.OnLoad(node);
+            setPattern();
+            setColor();
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            ConfigNode c1 = new ConfigNode("color1");
+            c1.AddValue("red", color1.r);
+            c1.AddValue("green", color1.g);
+            c1.AddValue("blue", color1.b);
+            node.AddNode(c1);
+            ConfigNode c2 = new ConfigNode("color2");
+            c2.AddValue("red", color2.r);
+            c2.AddValue("green", color2.g);
+            c2.AddValue("blue", color2.b);
+            node.AddNode(c2);
+            base.OnSave(node);
+        }
+
         /// <summary>
         /// Called every frame.
         /// </summary>
         public void Update()
         {
-            if (switchColor != swtclr)
+            if (switchColor != swtclr && HighLogic.LoadedSceneIsEditor)
             {
                 if (switchColor)
                 {
-                    red = red1;
-                    green = green1;
-                    blue = blue1;
-                    swtclr = switchColor;
+                    red = color1.r;
+                    green = color1.g;
+                    blue = color1.b;
                 }
                 else
                 {
-                    red = red2;
-                    green = green2;
-                    blue = blue2;
-                    swtclr = switchColor;
+                    red = color2.r;
+                    green = color2.g;
+                    blue = color2.b;
                 }
+                swtclr = switchColor;
             }
-            if ((HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight) &&
-                ((switchColor ? red != red1 || green != green1 || blue != blue1 :
-                red != red2 || green != green2 || blue != blue2) || 
+            if (HighLogic.LoadedSceneIsEditor && ((
+                switchColor ? red != color1.r || green != color1.g || blue != color1.b :
+                red != color2.r || green != color2.g || blue != color2.b) ||
                 (!texturePattern.Equals(texpat) || textureRepeat != texRep)))
             {
                 setPattern();
@@ -107,18 +139,18 @@ namespace KSPModelRocketry
                 texRend = part.FindModelComponent<Renderer>(textureMeshName);
                 mat = new Material(texRend.material.shader);
             }
-            if (switchColor)
-            {
-                red1 = red;
-                green1 = green;
-                blue1 = blue;
-            }
-            else
-            {
-                red2 = red;
-                green2 = green;
-                blue2 = blue;
-            }
+                if (switchColor)
+                {
+                    color1.r = red;
+                    color1.g = green;
+                    color1.b = blue;
+                }
+                else
+                {
+                    color2.r = red;
+                    color2.g = green;
+                    color2.b = blue;
+                }
             Texture2D tex = new Texture2D(2 * (int)textureRepeat, 2);
             Color[] colors = new Color[tex.height * tex.width];
             for (int i = 0; i < 2; i++)
@@ -130,11 +162,11 @@ namespace KSPModelRocketry
                         int index = (2 * k) + j + (2 * i * (int)textureRepeat);
                         if (pattern[i, j])
                         {
-                            colors[index] = new Color(red2, green2, blue2);
+                            colors[index] = color2;
                         }
                         else
                         {
-                            colors[index] = new Color(red1, green1, blue1);
+                            colors[index] = color1;
                         }
 
                     }
